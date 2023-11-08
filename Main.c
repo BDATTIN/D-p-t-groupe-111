d9ebf817ca1b5d0a9e3cd07a5982fadc60b98ea7
EthanMuscat <150148699+EthanMuscat@users.noreply.github.com>
2023-11-08 20:08:19 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,13 +4,17 @@
 #include <conio.h>
 #include <windows.h>
 #include "time.h"
-
 #include "Bibliothèque_SousProgrammes.h"
+#include <stdbool.h>
+#define LIGNES 10
+#define COLONNES 20
 
-int main() {
 
-    creationMatrice();
-    afficherHorloge();
+int main()
+{
+
+    _beginthread(afficherHorloge, 0, NULL);
+    afficherSNOOPY();
 
     return 0;
 }
